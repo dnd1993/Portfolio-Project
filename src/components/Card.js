a177c48dc2de30d8f1dc5afa55755b39bd540b89
Yana Nazarenko <dnd1993@yandex.ru>
2021-07-30 20:54:15 -0400

    updated files

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,21 @@
 import React from "react";
+import doughnut from "../img/doughnut.png";
 
 import "./Card.css";
 
 const Card = () => {
-  return <article className="project-card">Project</article>;
+  return (
+    <article className="project-card">
+      <div className="card-front">
+        <img src={doughnut} alt="Doughnut Logo" />
+        <h2>Filter Project</h2>
+      </div>
+      <div className="card-back">
+        <p>Project description</p>
+        <button>Visit Website</button>
+      </div>
+    </article>
+  );
 };
 
 export default Card;
