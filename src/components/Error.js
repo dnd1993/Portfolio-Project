5bef10d11f552122010ec1d392fd3b06bfd633aa
Yana Nazarenko <dnd1993@yandex.ru>
2021-08-03 18:02:36 -0400

    Update files

--- src/components/Error.js
+++ src/components/Error.js
@@ -2,6 +2,8 @@ import React from "react";
 
 export const Error = () => {
   return (
-    <p>This page doesn't exist. Please choose one of the sections above.</p>
+    <p>
+      Oops, this page doesn't exist. Please choose one of the sections above.
+    </p>
   );
 };
